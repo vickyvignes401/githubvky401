7bf2e3f003b690abbf2849cfe8bad3a0e838daac
vickyvignes401 <vickyvignes480@gmail.com>
2026-09-22 11:54:13 +0530

    to add a set function

---
+++ set.py
@@ -0,0 +1,17 @@
+ #set  .......>  do not allow duplicate
+#any data type will stored
+#set in ordered
+#cannot modify
+ #add(),update(),remve().pop()
+
+
+
+
+a={1,5,9,0,5,9,0,7,8,6,9}
+a.add(90)
+a.update([77])
+a.pop() #pop is not define any value only using
+a.remove(1)
+
+print(type(a))      
+print(a)    
